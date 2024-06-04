756059a3182eb3a3e12a36a9c4d2a3fdf8126ed2
Netero03 <jatinletsgo@gmail.com>
2024-06-04 21:40:04 +0530

    14th commit

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,3 @@ export { default as AboutUsSectionHome } from './AboutUsSectionHome';
 export { default as FilmSectionHome } from './FilmSectionHome';
 export { default as TeamSectionHome } from './TeamSectionHome';
 export { default as ScrollToTop } from './ScrollToTop';
-export { default as MaskedCursor } from './MaskedCursor';
