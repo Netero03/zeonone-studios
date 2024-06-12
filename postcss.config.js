3beb07e10bbb5e027da7ae078ed1c87f0f71aec9
Netero03 <jatinletsgo@gmail.com>
2024-06-12 17:04:52 +0530

    15th commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-import purgecssModule from '@fullhuman/postcss-purgecss';
-
-const purgecss = purgecssModule({
-  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
 
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...process.env.NODE_ENV === 'production' ? [purgecss] : [],
   },
 }
