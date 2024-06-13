9639ab9bdf157b0db3df4b9d52f16f6ae8142ab3
Netero03 <jatinletsgo@gmail.com>
2024-06-13 10:07:44 +0530

    15th commit

--- src/components/FilmSectionHome.jsx
+++ src/components/FilmSectionHome.jsx
@@ -195,7 +195,7 @@ const FilmSectionHome = () => {
             <div className="flex flex-col md:flex-row items-center px-3 md:justify-start w-full md:pl-60 z-20">
                 <div className="flex flex-row items-center text-center justify-center md:mx-0 mx-3 w-full md:h-[100px] h-[100px]">
                     <div className="h-5 w-1 bg-[#1C39BB] mt-1 mr-1 z-20"></div>
-                    <h2 className="font-bold md:text-[20px] text-[#373D3B] text-[20px] md:px-10 poppins-bold z-20"><Zoom duration={300} delay={-2000} >About Us</Zoom></h2>
+                    <h2 className="font-bold md:text-[20px] text-[#373D3B] text-[20px] md:px-10 poppins-bold z-20"><Zoom duration={300} delay={-2000} >Our Porjects</Zoom></h2>
                 </div>
                 <Link to="/films" className="arrow-link dancing-script-bold animate-pulse w-32 h-auto md:mr-28 rounded-md text-center text-2xl text-[#ffffff] self-center md:ml-auto md:mt-0 md:mb-0 mb-5 transition-all duration-300 hover:text-lg">
                     See more
