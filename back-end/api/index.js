7821a1b1c885d1ab16dfe44c682f1085ead64a0b
Netero03 <jatinletsgo@gmail.com>
2024-06-21 20:44:48 +0530

    17th commit

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get(`${PORT}/api/contact`, (req, res) => res.send("Express on Vercel"));
+app.get(`/`, (req, res) => res.send("Express on Vercel"));
 // Route to handle contact form submission
-app.post('/api/contact', (req, res) => {
+app.post('/', (req, res) => {
   const { name, email, message } = req.body;
 
   // Set up Nodemailer transport
