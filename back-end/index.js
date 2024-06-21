73caab09ff41fe5a0b768518628243a9dbec0550
Netero03 <jatinletsgo@gmail.com>
2024-06-21 20:06:41 +0530

    17th commit

--- back-end/index.js
+++ back-end/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/contact", (req, res) => res.send("Express on Vercel"));
 // Route to handle contact form submission
 app.post('/api/contact', (req, res) => {
   const { name, email, message } = req.body;
